53dca8e145246e18c5eedd7901876a5faec2b550
Randy Torres <torresr14@gmail.com>
2024-12-19 20:38:27 -0800

    .

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 from typing import Dict, Any
 import logging
 
-
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
